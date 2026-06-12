e2da77eda4b47107a797eb43c3744fc7dc5c645d
MeysamHos <meysamiam@gmail.com>
2026-06-12 09:40:59 +0330

    using env and gitignore

--- neydjango/greenhouse/settings.py
+++ neydjango/greenhouse/settings.py
@@ -14,10 +14,13 @@ from pathlib import Path
 from datetime import timedelta
 import dj_database_url
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load the .env file from your project root folder
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
